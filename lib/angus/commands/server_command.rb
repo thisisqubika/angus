f1e17bd89f50bfa1587e01cfa75f6329e86ada9b
lucas <lucas.aragno157@gmail.com>
2018-03-30 13:22:41 -0300

    Add flags to server command

--- lib/angus/commands/server_command.rb
+++ lib/angus/commands/server_command.rb
@@ -1,8 +1,15 @@
 module Angus
   class ServerCommand < Thor::Group
+    include Thor::Actions
+
+    class_option :port, desc: 'use PORT (default: 9292)', type: :string, required: false
+    class_option :host, desc: 'listen on HOST (default: localhost)', type: :string, required: false
 
     def server
-      command_processor.run('rackup', verbose: false)
+      port_option = "-p #{options[:port]}" || ''
+      host_option = "--host #{options[:host]}" || ''
+      puts "rackup #{port_option} #{host_option}"
+      command_processor.run("rackup #{port_option} #{host_option}", verbose: false)
     end
 
     private
